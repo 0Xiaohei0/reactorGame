21296c24e4550b0d53fb3f4374424c03c5d676f1
Sicheng Yi <15852786007ysc@gmail.com>
2022-05-11 10:13:26 -0400

    initial commit

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroid;
+    public float speedLower = -10.0f;
+    public float speedUpper = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnAsteroid", 0, 1f);
+    }
+
+    private void SpawnAsteroid()
+    {
+        GameObject createdAsteroid = Instantiate(asteroid, transform.position, transform.rotation);
+        createdAsteroid.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(speedLower, speedUpper), Random.Range(speedLower, speedUpper)));
+    }
+}
